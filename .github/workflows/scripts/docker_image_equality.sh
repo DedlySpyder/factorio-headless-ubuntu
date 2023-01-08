f2bc385cd0f5cd84b311b3a79b729b767c4d1f6f
DedlySpyder <dedlyspyder@gmail.com>
2023-01-07 22:45:16 -0500

    Added build on new factorio version workflow

---
+++ .github/workflows/scripts/docker_image_equality.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+set -exo pipefail
+
+REPO="dedlyspyder"
+IMAGE="factorio-headless-ubuntu"
+
+if [[ -z "$2" ]]; then
+  echo "::error Usage: $0 [factorio version] [headless type: stable|experimental] "
+  exit 1
+fi
+factorio_version="$1"
+headless_type="$2"
+
+script_version="$(git describe --tags --abbrev=0)"
+version_tag="${factorio_version}-${script_version}"
+
+docker pull "$REPO/${IMAGE}:$version_tag"
+docker pull "$REPO/${IMAGE}:$headless_type"
+
+if diff <(docker inspect "$REPO/${IMAGE}:$headless_type" | jq '.[].RootFS') <(docker inspect "$REPO/${IMAGE}:$version_tag" | jq '.[].RootFS') > /dev/null; then
+  echo "equals=true" >> $GITHUB_OUTPUT
+else
+  echo "equals=false" >> $GITHUB_OUTPUT
+fi
