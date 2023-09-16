81ad729140d220d69c05908a5d8921709d092b67
DedlySpyder <dedlyspyder@gmail.com>
2023-09-16 12:35:44 -0400

    Mod handling: Replaced apply_to_leaves with list_source_mod_files

--- factorio_headless/lib/mod_handling.py
+++ factorio_headless/lib/mod_handling.py
@@ -2,12 +2,45 @@ import os
 
 from collections.abc import Callable
 
-MODS_ROOT='/opt/headless_factorio/mods'
+MODS_SRC='/opt/headless_factorio/mods'
+MODS_DST='/opt/headless_factorio/game/mods'
 
 
-def apply_to_leaves(trunk: str, func: Callable[[str], None]):
+def strip_source(file: str, src: str) -> str:
+    """Strip the source of a file from the file path, plus 1 additional directory
+
+    Args:
+        file (str): The file to modify
+        src (str): The root source of the file
+
+    Returns:
+        str: Modified file string
+    
+    Example:
+        file: '/opt/headless_factorio/mods/source/mod_1/info.json'
+        src: '/opt/headless_factorio/mods'
+        
+        return: 'mod_1/info.json'
+    """
+    temp = file.removeprefix(src)
+    part_split = 1 if src.endswith('/') else 2
+    return os.sep.join(temp.split(os.sep)[part_split:])
+    
+
+def list_source_mod_files(trunk: str, dst: str) -> list[tuple[str, str]]:
+    all_files = []
     for root, _, files in os.walk(trunk):
         for f in files:
-            func(os.path.join(root, f))
+            src_f = os.path.join(root, f)
+            dst_f = os.path.join(dst, strip_source(src_f, trunk))
+            all_files.append((src_f, dst_f))
+    return all_files
 
-# def merge_mods(root_dir=MODS_ROOT):
+
+# def link_file(src: str, dst_root: str):
+    
+    
+
+# def merge_mods(root_src_dir=MODS_SRC, root_dst_dir=MODS_DST) -> None:
+
+    
--- tests/mod_handling_test.py
+++ tests/mod_handling_test.py
@@ -14,34 +14,73 @@ class TestModHandling(unittest.TestCase):
     def tearDownClass(self):
         self.res.cleanup()
     
-    def test_apply_to_leaves__only_files_for_func(self):
-        files: list[str] = []
-        def func(s):
-            files.append(s)
-        mod_handling.apply_to_leaves(self.res.mods_root, func)
+    def test__strip_source__example_use_case(self):
+        file = '/opt/headless_factorio/mods/source/mod_1/info.json'
+        src = '/opt/headless_factorio/mods'
+        expected = 'mod_1/info.json'
+        
+        actual = mod_handling.strip_source(file, src)
+        self.assertEqual(actual, expected)
+    
+    def test__strip_source__correct_mod_info(self):
+        file = '/root/should_be_removed/mod_1/info.json'
+        src = '/root'
+        expected = 'mod_1/info.json'
+        
+        actual = mod_handling.strip_source(file, src)
+        self.assertEqual(actual, expected)
+    
+    def test__strip_source__correct_mod_zip(self):
+        file = '/root/should_be_removed/mod_1.zip'
+        src = '/root'
+        expected = 'mod_1.zip'
+        
+        actual = mod_handling.strip_source(file, src)
+        self.assertEqual(actual, expected)
+    
+    def test__strip_source__correct_mod_data(self):
+        file = '/root/should_be_removed/mod_1/data.lua'
+        src = '/root'
+        expected = 'mod_1/data.lua'
+        
+        actual = mod_handling.strip_source(file, src)
+        self.assertEqual(actual, expected)
+    
+    def test__strip_source__correct_mod_deep_file(self):
+        file = '/root/should_be_removed/mod_1/foo/bar/baz/buz.lua'
+        src = '/root'
+        expected = 'mod_1/foo/bar/baz/buz.lua'
+        
+        actual = mod_handling.strip_source(file, src)
+        self.assertEqual(actual, expected)
+    
+    def test__strip_source__src_endswith_slash(self):
+        file = '/root/should_be_removed/file'
+        src = '/root/'
+        expected = 'file'
+        
+        actual = mod_handling.strip_source(file, src)
+        self.assertEqual(actual, expected)
+    
+    def test__list_source_mod_files__found_only_files_for_func(self):
+        files = mod_handling.list_source_mod_files(self.res.mods_root, '.')
         for f in files:
-            self.assertTrue(pathlib.Path(f).is_file(), f'Leaf is not a file: {f}')
+            self.assertTrue(pathlib.Path(f[0]).is_file(), f'Leaf is not a file: {f}')
             
-    def test_apply_to_leaves__file_counts_total(self):
-        files: list[str] = []
-        def func(s):
-            files.append(s)
-        mod_handling.apply_to_leaves(self.res.mods_root, func)
+    def test__list_source_mod_files__file_counts_total(self):
+        files = mod_handling.list_source_mod_files(self.res.mods_root, '.')
         self.assertEqual(len(files), 4, "Unexpected count of leaf files")
             
-    def test_apply_to_leaves__file_counts_by_type(self):
-        files: list[str] = []
-        def func(s):
-            files.append(s)
-        mod_handling.apply_to_leaves(self.res.mods_root, func)
+    def test__list_source_mod_files__file_counts_by_type(self):
+        files = mod_handling.list_source_mod_files(self.res.mods_root, '.')
         zips, infos = 0, 0
         for f in files:
-            if f.endswith('.zip'):
+            if f[0].endswith('.zip'):
                 zips += 1
-            elif f.endswith('info.json'):
+            elif f[0].endswith('info.json'):
                 infos += 1
             else:
-                raise AssertionError(f'Unexpected file type: {f}')
+                raise AssertionError(f'Unexpected file type: {f[0]}')
         self.assertEqual(zips, 2, "Unexpected count of leaf files")
         self.assertEqual(infos, 2, "Unexpected count of leaf files")
         
