ab0c30eb9053c43494391a3b8b9398f512db0b57
DedlySpyder <dedlyspyder@gmail.com>
2023-09-19 23:36:40 -0400

    Tests: Changed build_test_dir to automatically find the calling function's name

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,3 +1,4 @@
+import inspect
 import os
 import pathlib
 import shutil
@@ -11,10 +12,11 @@ class TestResources():
         self.temp_dir = os.path.join(MODS_TEMP_ROOT, mod_name)
         pathlib.Path(self.temp_dir).mkdir(parents=True, exist_ok=True)
     
-    def build_test_dir(self, test_name) -> None:
+    def build_test_dir(self) -> str:
+        test_name = inspect.stack()[1][3] # Get the calling function's name
         test_dir = os.path.join(self.temp_dir, test_name)
-        if not os.path.exists(test_dir):
-            os.mkdir(test_dir)
+        pathlib.Path(test_dir).mkdir(exist_ok=True)
+        return test_dir
     
     def cleanup(self) -> None:
         shutil.rmtree(self.temp_dir)
